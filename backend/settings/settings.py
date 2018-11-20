2725e2a96e6831ae3d52d3dd11ea607f4d669979
soubhagya <soubhagyakumar666@gmail.com>
2018-11-21 02:15:53 +0530

    added docker branch

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -112,11 +112,12 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'rango',
+        'PORT': 27017,
+        'HOST': 'mongo'
     }
 }
-
 
 
 
